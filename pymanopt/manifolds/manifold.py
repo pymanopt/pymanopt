5e643175e83eac49a22070ec009691e2768e3ab4
Jamie Townsend <jamiehntownsend@gmail.com>
2015-11-17 14:45:08 +0000

    Added NotImplementedError to manifold.py abstract base class

--- pymanopt/manifolds/manifold.py
+++ pymanopt/manifolds/manifold.py
@@ -9,21 +9,21 @@ class Manifold(object):
     def retr(self, X, G):
         # A retraction mapping from the tangent space at X to the manifold.
         # See Absil for definition of retraction.
-        return
+        raise NotImplementedError()
 
     @abc.abstractmethod
     def egrad2rgrad(self, X, G):
         # A mapping from the Euclidean gradient G into the tangent space
         # to the manifold at X.
-        return
+        raise NotImplementedError()
     
     @abc.abstractmethod
     def norm(self, X, G):
         # Compute the norm of a tangent vector G, which is tangent to the
         # manifold at X.
-        return
+        raise NotImplementedError()
         
     @abc.abstractmethod
     def rand(self):
         # A function which returns a random point on the manifold.
-        return
+        raise NotImplementedError()
