d7b201d6bdded5f4a6b6c9766877bbe52273eb0a
Jamie Townsend <jamiehntownsend@gmail.com>
2015-11-17 12:32:50 +0000

    Added setup.py, requirements.txt and .travis.yml

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='pymanopt',
+      version='0.1',
+      description='Python port of the manopt MATLAB package, using Theano for autodiff.',
+      url='https://github.com/j-towns/pymanopt',
+      author='Jamie Townsend',
+      author_email='jamiehntownsend@gmail.com',
+      license='GNU2',
+      packages=find_packages(),
+        )
