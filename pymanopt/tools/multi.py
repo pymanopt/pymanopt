35797f5ed317652b8f8a5ac07e55201f3d51082e
Jamie Townsend <jamiehntownsend@gmail.com>
2016-02-05 17:05:42 +0100

    Add theano tests for matrices

--- pymanopt/tools/multi.py
+++ pymanopt/tools/multi.py
@@ -3,11 +3,13 @@ import numpy as np
 
 
 def multiprod(A, B):
-    # Inspired by MATLAB multiprod function by Paolo de Leva. A and B are
-    # assumed to be arrays containing M matrices, that is, A and B have
-    # dimensions A: (M, N, P), B:(M, P, Q). multiprod multiplies each matrix
-    # in A with the corresponding matrix in B, using matrix multiplication.
-    # so multiprod(A, B) has dimensions (M, N, Q).
+    """
+    Inspired by MATLAB multiprod function by Paolo de Leva. A and B are
+    assumed to be arrays containing M matrices, that is, A and B have
+    dimensions A: (M, N, P), B:(M, P, Q). multiprod multiplies each matrix
+    in A with the corresponding matrix in B, using matrix multiplication.
+    so multiprod(A, B) has dimensions (M, N, Q).
+    """
 
     # First check if we have been given just one matrix
     if len(np.shape(A)) == 2:
@@ -19,12 +21,14 @@ def multiprod(A, B):
 
 
 def multitransp(A):
-    # Inspired by MATLAB multitransp function by Paolo de Leva. A is assumed to
-    # be an array containing M matrices, each of which has dimension N x P.
-    # That is, A is an M x N x P array. Multitransp then returns an array
-    # containing the M matrix transposes of the matrices in A, each of which
-    # will be P x N.
-
+    """
+    Inspired by MATLAB multitransp function by Paolo de Leva. A is assumed to
+    be an array containing M matrices, each of which has dimension N x P.
+    That is, A is an M x N x P array. Multitransp then returns an array
+    containing the M matrix transposes of the matrices in A, each of which
+    will be P x N.
+    """
+    
     # First check if we have been given just one matrix
     if len(np.shape(A)) == 2:
         return A.T
---
+++ tests/test_theano.py
@@ -0,0 +1,76 @@
+import unittest
+
+import numpy as np
+import numpy.linalg as la
+import numpy.random as rnd
+import numpy.testing as np_testing
+
+import theano.tensor as T
+
+from pymanopt.tools.theano_functions import *
+
+
+class TestMatrix(unittest.TestCase):
+    def setUp(self):
+        self.X = X = T.matrix()
+        self.cost = T.exp(T.sum(X**2))
+
+        m = self.m = 10
+        n = self.n = 15
+
+        Y = self.Y = rnd.randn(m, n)
+        A = self.A = rnd.randn(m, n)
+
+        # Calculate correct cost and grad...
+        self.correct_cost = np.exp(np.sum(Y ** 2))
+        self.correct_grad = correct_grad = ((2 * np.ones((m, n)) * Y *
+                                             np.exp(np.sum(Y ** 2))))
+
+        # ... and hess
+        # First form hessian tensor H (4th order)
+        Y1 = Y.reshape(m, n, 1, 1)
+        Y2 = Y.reshape(1, 1, m, n)
+
+        # Create an m x n x m x n array with diag[i,j,k,l] == 1 iff
+        # (i == k and j == l), this is a 'diagonal' tensor.
+        diag = np.eye(m * n).reshape(m, n, m, n)
+
+        H = np.exp(np.sum(Y ** 2)) * (4 * Y1 * Y2 + 2 * diag)
+
+        # Then 'right multiply' H by A
+        Atensor = A.reshape(1, 1, m, n)
+
+        self.correct_hess = np.sum(H * Atensor, axis=(2,3))
+
+    def test_compile(self):
+        cost_compiled = compile(self.cost, self.X)
+        np_testing.assert_allclose(self.correct_cost, cost_compiled(self.Y))
+
+    def test_grad(self):
+        grad = gradient(self.cost, self.X)
+        np_testing.assert_allclose(self.correct_grad, grad(self.Y))
+
+    def test_grad_hess(self):
+        grad, hess = grad_hess(self.cost, self.X)
+
+        # First test grad as above
+        np_testing.assert_allclose(self.correct_grad, grad(self.Y))
+
+        # Now test hess
+        np_testing.assert_allclose(self.correct_hess, hess(self.Y, self.A))
+
+    def test_grad_hess_no_Rop(self):
+        # Break the Rop in T.exp
+        def new_Rop(x, y):
+            raise NotImplementedError
+
+        T.exp.R_op = new_Rop
+
+        # Rebuild graph to force recompile
+        X = T.matrix()
+        cost = T.exp(T.sum(X**2))
+
+        # And check that all is still well
+        grad, hess = grad_hess(cost, X)
+
+        np_testing.assert_allclose(self.correct_hess, hess(self.Y, self.A))
