7fc52f08ae0332a5ce164c6844000f8b8c4cb004
Niklas Koep <niklas.koep@gmail.com>
2016-02-15 16:05:22 +0100

    manifold.py: simplify abstract methods/properties
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/manifolds/manifold.py
+++ pymanopt/manifolds/manifold.py
@@ -10,86 +10,86 @@ class Manifold(object):
 
     @abc.abstractproperty
     def name(self):
-        # Name of the manifold.
-        raise NotImplementedError
+        # Name of the manifold
+        pass
 
     @abc.abstractproperty
     def dim(self):
         # Dimension of the manifold
-        raise NotImplementedError
+        pass
 
     @abc.abstractproperty
     def typicaldist(self):
         # Returns the "scale" of the manifold. This is used by the
         # trust-regions solver, to determine default initial and maximal
         # trust-region radii.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def dist(self, X, Y):
         # Geodesic distance on the manifold
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def inner(self, X, G, H):
         # Inner product (Riemannian metric) on the tangent space
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def proj(self, X, G):
         # Project into the tangent space. Usually the same as egrad2rgrad
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def ehess2rhess(self, X, Hess):
         # Convert Euclidean hessian into Riemannian hessian.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def retr(self, X, G):
         # A retraction mapping from the tangent space at X to the manifold.
         # See Absil for definition of retraction.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def egrad2rgrad(self, X, G):
         # A mapping from the Euclidean gradient G into the tangent space
         # to the manifold at X.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def norm(self, X, G):
         # Compute the norm of a tangent vector G, which is tangent to the
         # manifold at X.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def rand(self):
         # A function which returns a random point on the manifold.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def randvec(self, X):
         # Returns a random, unit norm vector in the tangent space at X.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def transp(self, x1, x2, d):
         # Transports d, which is a tangent vector at x1, into the tangent
         # space at x2.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def exp(self, X, U):
         # The exponential (in the sense of Lie group theory) of a tangent
         # vector U at X.
-        raise NotImplementedError
+        pass
 
     @abc.abstractmethod
     def log(self, X, Y):
         # The logarithm (in the sense of Lie group theory) of Y. This is the
         # inverse of exp.
-        raise NotImplementedError
+        pass
 
     def zerovec(self, X):
         """
