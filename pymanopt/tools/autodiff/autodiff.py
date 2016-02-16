e7ec8ec2e3d51a0f139a9ee08a6d491921e8c380
Niklas Koep <niklas.koep@gmail.com>
2016-02-16 17:35:57 +0100

    autodiff.py: raise ValueError for invalid input
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/tools/autodiff/autodiff.py
+++ pymanopt/tools/autodiff/autodiff.py
@@ -1,4 +1,6 @@
-from warnings import warn
+import theano.tensor as T
+
+from pymanopt.tools.autodiff import _theano, _autograd
 
 
 def compile(problem, need_grad, need_hess):
@@ -6,25 +8,25 @@ def compile(problem, need_grad, need_hess):
     if (problem.cost is None or
        (need_grad and problem.grad is None and problem.egrad is None) or
        (need_hess and problem.hess is None and problem.ehess is None)):
-        if type(problem.ad_cost).__name__ == 'TensorVariable':
-            from pymanopt.tools.autodiff import _theano as ad
-        elif type(problem.ad_cost).__name__ == 'function':
-            from pymanopt.tools.autodiff import _autograd as ad
+        if isinstance(problem.ad_cost, T.TensorVariable):
+            backend = _theano
+        elif callable(problem.ad_cost):
+            backend = _autograd
         else:
-            warn('Cannot identify autodiff backend from '
-                 'ad_cost variable type.')
+            raise ValueError("Cannot identify autodiff backend from "
+                             "ad_cost variable type.")
 
     if problem.verbosity >= 1:
         print("Compiling cost function...")
     if problem.cost is None:
-        problem.cost = ad.compile(problem.ad_cost, problem.ad_arg)
+        problem.cost = backend.compile(problem.ad_cost, problem.ad_arg)
 
     if need_grad and problem.egrad is None and problem.grad is None:
         if problem.verbosity >= 1:
             print("Computing gradient of cost function...")
-        problem.egrad = ad.gradient(problem.ad_cost, problem.ad_arg)
+        problem.egrad = backend.gradient(problem.ad_cost, problem.ad_arg)
         # Assume if Hessian is needed gradient is as well
         if need_hess and problem.ehess is None and problem.hess is None:
             if problem.verbosity >= 1:
                 print("Computing Hessian of cost function...")
-            problem.ehess = ad.hessian(problem.ad_cost, problem.ad_arg)
+            problem.ehess = backend.hessian(problem.ad_cost, problem.ad_arg)
