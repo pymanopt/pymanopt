afe0e5a42d337ed6c764e0c891584ebfb2197d06
sweichwald <git@sweichwald.de>
2016-02-18 12:43:08 +0100

    Outline sphere manifold tests.

--- tests/test_manifold_sphere.py
+++ tests/test_manifold_sphere.py
@@ -12,16 +12,59 @@ class TestSphereManifold(unittest.TestCase):
     def setUp(self):
         self.m = m = 100
         self.n = n = 50
-        self.sphere = Sphere(m, n)
+        self.man = Sphere(m, n)
+
+    # def test_dim(self):
+
+    # def test_typicaldist(self):
+
+    # def test_dist(self):
+
+    # def test_inner(self):
 
     def test_proj(self):
-        # Construct a random point X on the manifold.
+        #  Construct a random point X on the manifold.
         X = rnd.randn(self.m, self.n)
         X /= la.norm(X, "fro")
 
-        # Construct a vector H in the ambient space.
+        #  Construct a vector H in the ambient space.
         H = rnd.randn(self.m, self.n)
 
-        # Compare the projections.
+        #  Compare the projections.
         np_testing.assert_array_almost_equal(H - X * np.trace(X.T.dot(H)),
-                                             self.sphere.proj(X, H))
+                                             self.man.proj(X, H))
+
+    # def test_ehess2rhess(self):
+
+    # def test_retr(self):
+
+    # def test_egrad2rgrad(self):
+
+    # def test_norm(self):
+
+    # def test_rand(self):
+
+    # def test_randvec(self):
+
+    # def test_transp(self):
+
+    def test_exp_log_inverse(self):
+        s = self.man
+        X = s.rand()
+        U = s.randvec(X)
+        Uexplog = s.exp(X, s.log(X, U))
+        np_testing.assert_array_almost_equal(U, Uexplog)
+
+    def test_log_exp_inverse(self):
+        s = self.man
+        X = s.rand()
+        U = s.randvec(X)
+        Ulogexp = s.log(X, s.exp(X, U))
+        np_testing.assert_array_almost_equal(U, Ulogexp)
+
+    def test_pairmean(self):
+        s = self.man
+        X = s.rand()
+        Y = s.rand()
+        Z = s.pairmean(X, Y)
+        np_testing.assert_array_almost_equal(s.dist(X, Z), s.dist(Y, Z))
