acb298360d465418dffa98d763e46be0d38f4b60
Jamie Townsend <jamiehntownsend@gmail.com>
2016-03-04 18:12:15 +0000

    Add docstring to PositiveDefinite manifold.

--- pymanopt/manifolds/psd.py
+++ pymanopt/manifolds/psd.py
@@ -12,7 +12,12 @@ from pymanopt.tools.multi import *
 
 class PositiveDefinite(Manifold):
     '''
-    Abstract base class setting out template for manifold classes.
+    Manifold of (n x n)^k positive definite matrices, based on the geometry 
+    discussed in Chapter 6 of Positive Definite Matrices (Bhatia 2007). Some 
+    of the implementation is based on sympositivedefinitefactory.m from the 
+    Manopt MATLAB package. Also see "Conic geometric optimisation on the 
+    manifold of positive definite matrices" (Sra & Hosseini 2013) for more 
+    details.
     '''
     def __init__(self, n, k=1):
         self._n = n
