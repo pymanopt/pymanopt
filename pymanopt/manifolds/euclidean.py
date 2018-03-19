02130f5dde37eea8faff0b48255bffdfb69918af
Andrey Afonin <yota-7@yandex.ru>
2018-03-19 19:07:50 +0300

    Added manifold of skew-symmetric matrices
    Signed-off-by: Andrey Afonin <yota-7@yandex.ru>

--- pymanopt/manifolds/euclidean.py
+++ pymanopt/manifolds/euclidean.py
@@ -6,6 +6,7 @@ import numpy.random as rnd
 
 from pymanopt.manifolds.manifold import Manifold
 from pymanopt.tools.multi import multisym
+from pymanopt.tools.multi import multiskew
 
 
 class Euclidean(Manifold):
@@ -100,10 +101,12 @@ class Symmetric(Euclidean):
             self._shape = (n, n)
             self._name = ("Manifold of {} x {} symmetric matrices."
                           ).format(n, n)
-        else:
+        elif k > 1:
             self._shape = (k, n, n)
             self._name = ("Product manifold of {} ({} x {}) symmetric "
                           "matrices.").format(k, n, n)
+        else:
+            raise ValueError("k must be an integer no less than 1.")
 
         self._dim = 0.5 * k * n * (n + 1)
 
@@ -129,3 +132,49 @@ class Symmetric(Euclidean):
     def randvec(self, X):
         Y = self.rand()
         return multisym(Y / self.norm(X, Y))
+
+
+class SkewSymmetric(Euclidean):
+    """
+    The Euclidean space of n-by-n skew-symmetric matrices.
+
+    If k > 1 then this is an array of shape (k, n, n) (product manifold)
+    containing k (n x n) matrices.
+    """
+
+    def __init__(self, n, k=1):
+        if k == 1:
+            self._shape = (n, n)
+            self._name = ("Manifold of {} x {} skew-symmetric matrices."
+                          ).format(n, n)
+        elif k > 1:
+            self._shape = (k, n, n)
+            self._name = ("Product manifold of {} ({} x {}) skew-symmetric "
+                          "matrices.").format(k, n, n)
+        else:
+            raise ValueError("k must be an integer no less than 1.")
+
+        self._dim = .5 * k * n * (n - 1)
+
+    def __str__(self):
+        return self._name
+
+    @property
+    def dim(self):
+        return self._dim
+
+    def proj(self, X, U):
+        return multiskew(U)
+
+    def egrad2rgrad(self, X, U):
+        return multiskew(U)
+
+    def ehess2rhess(self, X, egrad, ehess, H):
+        return multiskew(ehess)
+
+    def rand(self):
+        return multiskew(rnd.randn(*self._shape))
+
+    def randvec(self, X):
+        G = self.rand()
+        return multiskew(G / self.norm(X, G))
