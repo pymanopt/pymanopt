04de5c98a1823c96c28757f7aef007eb6044d88d
Niklas Koep <niklas.koep@gmail.com>
2018-05-07 14:53:59 +0200

    autodiff backends: make 'is_available' static
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/tools/autodiff/_autograd.py
+++ pymanopt/tools/autodiff/_autograd.py
@@ -2,11 +2,12 @@
 Module containing functions to differentiate functions using autograd.
 """
 try:
+    import autograd
+except ImportError:
+    autograd = None
+else:
     import autograd.numpy as np
     from autograd import grad
-except ImportError:
-    np = None
-    grad = None
 
 from ._backend import Backend, assert_backend_available
 
@@ -15,7 +16,8 @@ class AutogradBackend(Backend):
     def __str__(self):
         return "autograd"
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         return np is not None and grad is not None
 
     @assert_backend_available
@@ -25,11 +27,10 @@ class AutogradBackend(Backend):
     @assert_backend_available
     def compile_function(self, objective, argument):
         def func(x):
-            if type(x) in (list, tuple):
-                return objective([np.array(xi) for xi in x])
+            if isinstance(x, (list, tuple)):
+                return objective(list(map(np.array, x)))
             else:
                 return objective(np.array(x))
-
         return func
 
     @assert_backend_available
@@ -43,8 +44,8 @@ class AutogradBackend(Backend):
         # Sometimes x will be some custom type, e.g. with the FixedRankEmbedded
         # manifold. Therefore cast it to a numpy.array.
         def gradient(x):
-            if type(x) in (list, tuple):
-                return g([np.array(xi) for xi in x])
+            if isinstance(x, (list, tuple)):
+                return g(list(map(np.array, x)))
             else:
                 return g(np.array(x))
         return gradient
