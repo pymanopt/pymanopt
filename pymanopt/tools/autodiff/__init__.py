231eae413d8387c1c09055ff5c56d2cc2597729f
Niklas Koep <niklas.koep@gmail.com>
2018-05-07 14:51:40 +0200

    Add canonical Callable backend and decorators
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/tools/autodiff/__init__.py
+++ pymanopt/tools/autodiff/__init__.py
@@ -1,7 +1,12 @@
-from ._theano import TheanoBackend
-
+from ._callable import CallableBackend
 from ._autograd import AutogradBackend
-
+from ._pytorch import PyTorchBackend
+from ._theano import TheanoBackend
 from ._tensorflow import TensorflowBackend
+
+_BACKENDS = (CallableBackend, AutogradBackend, PyTorchBackend, TheanoBackend,
+             TensorflowBackend)
+__all__ = [Backend.__name__ for Backend in _BACKENDS]
+
 
 __all__ = ["TheanoBackend", "AutogradBackend", "TensorflowBackend"]
