57a3391c391cf55bf70e781453faa69d223161f4
Niklas Koep <niklas.koep@gmail.com>
2018-12-06 20:56:28 +0100

    Replace failing unit test due to backend changes
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -1,15 +1,18 @@
 import unittest
 
+import numpy as np
+from numpy import random as rnd
+import numpy.testing as np_testing
 import theano.tensor as T
 
-from pymanopt import Problem
+from pymanopt import Problem, TheanoFunction
 from pymanopt.manifolds import Sphere
 
 
 class TestProblem(unittest.TestCase):
     def setUp(self):
         self.X = X = T.vector()
-        self.cost = T.exp(T.sum(X**2))
+        self.cost = TheanoFunction(T.exp(T.sum(X**2)), X)
 
         n = self.n = 15
 
@@ -17,8 +20,6 @@ class TestProblem(unittest.TestCase):
 
     def test_prepare(self):
         problem = Problem(self.man, self.cost)
-        with self.assertRaises(ValueError):
-            # Asking for the gradient of a Theano cost function without
-            # specifying an argument for differentiation should raise an
-            # exception.
-            problem.grad
+        x = rnd.randn(self.n)
+        np_testing.assert_allclose(2 * x * np.exp(np.sum(x ** 2)),
+                                   problem.egrad(x))
