13043c707ca075cf05bae5a79a725464fa6b1654
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2019-03-12 18:11:30 +0100

    correcting dist for PSD
    Signed-off-by: Sylvain Chevallier <sylvain.chevallier@uvsq.fr>

--- pymanopt/manifolds/psd.py
+++ pymanopt/manifolds/psd.py
@@ -54,11 +54,11 @@ class PositiveDefinite(Manifold):
         # Adapted from equation 6.13 of "Positive definite matrices". Chol
         # decomp gives the same result as matrix sqrt. There may be a more
         # efficient way to compute this!
-        c = np.linalg.cholesky(x)
-        c_inv = np.linalg.inv(c)
+        c = la.cholesky(x)
+        c_inv = la.inv(c)
         logm = multilog(multiprod(multiprod(c_inv, y), multitransp(c_inv)),
                         pos_def=True)
-        return la.norm(multiprod(multiprod(c, logm), c_inv))
+        return la.norm(logm)
 
     def inner(self, x, u, v):
         return np.tensordot(la.solve(x, u), la.solve(x, v), axes=x.ndim)
