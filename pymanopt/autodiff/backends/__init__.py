ce56a63f341ca21ab7043ad211095c88c64a5d0f
Niklas Koep <niklas.koep@gmail.com>
2020-01-24 17:26:07 +0100

    backends: add '__all__' variable to package
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/autodiff/backends/__init__.py
+++ pymanopt/autodiff/backends/__init__.py
@@ -3,3 +3,6 @@ from ._callable import Callable
 from ._pytorch import PyTorch
 from ._tensorflow import Tensorflow
 from ._theano import Theano
+
+
+__all__ = ("Autograd", "Callable", "PyTorch", "Tensorflow", "Theano")
