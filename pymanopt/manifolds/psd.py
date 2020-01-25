9f1eb52fe65cc20cce50a9e046ff483215cf3bf2
Niklas Koep <niklas.koep@gmail.com>
2020-01-25 09:05:48 +0100

    psd.py: move docstring out of private base class
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/manifolds/psd.py
+++ pymanopt/manifolds/psd.py
@@ -156,38 +156,6 @@ class SymmetricPositiveDefinite(EuclideanEmbeddedSubmanifold):
 #              the next class to a dedicated 'psd_fixed_rank' module.
 
 class _PSDFixedRank(Manifold, _RetrAsExpMixin):
-    """
-    Manifold of n-by-n symmetric positive semidefinite matrices of rank k.
-
-    A point X on the manifold is parameterized as YY^T where Y is a matrix of
-    size nxk. As such, X is symmetric, positive semidefinite. We restrict to
-    full-rank Y's, such that X has rank exactly k. The point X is numerically
-    represented by Y (this is more efficient than working with X, which may
-    be big). Tangent vectors are represented as matrices of the same size as
-    Y, call them Ydot, so that Xdot = Y Ydot' + Ydot Y. The metric is the
-    canonical Euclidean metric on Y.
-
-    Since for any orthogonal Q of size k, it holds that (YQ)(YQ)' = YY',
-    we "group" all matrices of the form YQ in an equivalence class. The set
-    of equivalence classes is a Riemannian quotient manifold, implemented
-    here.
-
-    Notice that this manifold is not complete: if optimization leads Y to be
-    rank-deficient, the geometry will break down. Hence, this geometry should
-    only be used if it is expected that the points of interest will have rank
-    exactly k. Reduce k if that is not the case.
-
-    An alternative, complete, geometry for positive semidefinite matrices of
-    rank k is described in Bonnabel and Sepulchre 2009, "Riemannian Metric
-    and Geometric Mean for Positive Semidefinite Matrices of Fixed Rank",
-    SIAM Journal on Matrix Analysis and Applications.
-
-    The geometry implemented here is the simplest case of the 2010 paper:
-    M. Journee, P.-A. Absil, F. Bach and R. Sepulchre,
-    "Low-Rank Optimization on the Cone of Positive Semidefinite Matrices".
-    Paper link: http://www.di.ens.fr/~fbach/journee2010_sdp.pdf
-    """
-
     def __init__(self, n, k, name, dimension):
         self._n = n
         self._k = k
@@ -244,7 +212,37 @@ class _PSDFixedRank(Manifold, _RetrAsExpMixin):
 
 
 class PSDFixedRank(_PSDFixedRank):
-    __doc__ = _PSDFixedRank.__doc__
+    """
+    Manifold of n-by-n symmetric positive semidefinite matrices of rank k.
+
+    A point X on the manifold is parameterized as YY^T where Y is a matrix of
+    size nxk. As such, X is symmetric, positive semidefinite. We restrict to
+    full-rank Y's, such that X has rank exactly k. The point X is numerically
+    represented by Y (this is more efficient than working with X, which may
+    be big). Tangent vectors are represented as matrices of the same size as
+    Y, call them Ydot, so that Xdot = Y Ydot' + Ydot Y. The metric is the
+    canonical Euclidean metric on Y.
+
+    Since for any orthogonal Q of size k, it holds that (YQ)(YQ)' = YY',
+    we "group" all matrices of the form YQ in an equivalence class. The set
+    of equivalence classes is a Riemannian quotient manifold, implemented
+    here.
+
+    Notice that this manifold is not complete: if optimization leads Y to be
+    rank-deficient, the geometry will break down. Hence, this geometry should
+    only be used if it is expected that the points of interest will have rank
+    exactly k. Reduce k if that is not the case.
+
+    An alternative, complete, geometry for positive semidefinite matrices of
+    rank k is described in Bonnabel and Sepulchre 2009, "Riemannian Metric
+    and Geometric Mean for Positive Semidefinite Matrices of Fixed Rank",
+    SIAM Journal on Matrix Analysis and Applications.
+
+    The geometry implemented here is the simplest case of the 2010 paper:
+    M. Journee, P.-A. Absil, F. Bach and R. Sepulchre,
+    "Low-Rank Optimization on the Cone of Positive Semidefinite Matrices".
+    Paper link: http://www.di.ens.fr/~fbach/journee2010_sdp.pdf
+    """
 
     def __init__(self, n, k):
         name = ("YY' quotient manifold of {:d}x{:d} psd matrices of "
