a68a46ad0932c5c51f14a4bd0c95b6adf0aa503d
Niklas Koep <niklas.koep@gmail.com>
2020-01-25 17:27:03 +0100

    _theano.py: use more explicit variable names
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/autodiff/backends/_theano.py
+++ pymanopt/autodiff/backends/_theano.py
@@ -1,9 +1,3 @@
-"""
-Module containing functions to compile and differentiate Theano graphs. Part of
-the pymanopt package.
-
-Jamie Townsend December 2014
-"""
 import itertools
 
 try:
@@ -19,7 +13,7 @@ from .. import make_graph_backend_decorator
 from ...tools import unpack_arguments, flatten_args, group_return_values
 
 
-class TheanoBackend(Backend):
+class _TheanoBackend(Backend):
     def __str__(self):
         return "theano"
 
@@ -33,65 +27,65 @@ class TheanoBackend(Backend):
         return all([isinstance(arg, T.TensorVariable) for arg in args])
 
     @assert_backend_available
-    def compile_function(self, func, argument):
-        """
-        Wrapper for theano.function(). Compiles a theano graph into a python
-        function.
-        """
-        args = flatten_args(argument)
-        f = theano.function(inputs=args, outputs=func)
-        if len(args) == 1:
-            return f
-        return unpack_arguments(f)
+    def compile_function(self, function, arguments):
+        """Compiles a Theano graph into a python function."""
+        flattened_arguments = flatten_args(arguments)
+        compiled_function = theano.function(inputs=flattened_arguments,
+                                            outputs=function)
+        if len(flattened_arguments) == 1:
+            return compiled_function
+        return unpack_arguments(compiled_function)
 
     @assert_backend_available
-    def compute_gradient(self, func, argument):
+    def compute_gradient(self, function, arguments):
+        """Returns a compiled function computing the gradient of 'function'
+        with respect to 'arguments'.
         """
-        Returns a compiled function computing the gradient of 'func' with
-        respect to 'argument'.
-        """
-        args = flatten_args(argument)
+        flattened_arguments = flatten_args(arguments)
         # For cost functions expecting one argument, make sure we don't return
         # the gradient evaluated at some point as a singleton tuple.
-        if len(args) == 1:
-            (arg,) = args
-            g = T.grad(func, arg)
-            return theano.function(args, g)
+        if len(flattened_arguments) == 1:
+            (argument,) = flattened_arguments
+            gradient = T.grad(function, argument)
+            return theano.function(flattened_arguments, gradient)
 
-        g = T.grad(func, args)
-        grad = theano.function(args, g)
-        return group_return_values(unpack_arguments(grad), argument)
-
-    @staticmethod
-    def _compute_unary_hvp(g, argument):
-        """
-        Returns a function accepting two arguments to compute a Hessian-vector
-        product of a scalar-valued unary function.
-        """
-        u = argument.type()
-        try:
-            R = T.Rop(g, argument, u)
-        except NotImplementedError:
-            proj = T.sum(g * disconnected_grad(u))
-            R = T.grad(proj, argument)
-        return theano.function([argument, u], R, on_unused_input="warn")
+        gradient = T.grad(function, flattened_arguments)
+        compiled_gradient = theano.function(flattened_arguments, gradient)
+        return group_return_values(
+            unpack_arguments(compiled_gradient), arguments)
 
     @staticmethod
-    def _compute_nary_hvp(g, args):
+    def _compute_unary_hessian_vector_product(gradient, argument):
+        """Returns a function accepting two arguments to compute a
+        Hessian-vector product of a scalar-valued unary function.
         """
-        Returns a function accepting 2 * len(args) arguments to compute a
-        Hessian-vector product of a multivariate function defined on a product
-        manifold.
-        """
-        u = [arg.type() for arg in args]
+        argument_type = argument.type()
         try:
-            R = T.Rop(g, args, u)
+            Rop = T.Rop(gradient, argument, argument_type)
         except NotImplementedError:
-            # TODO(nkoep): Write a test case for this path.
-            # Implementation based on:
-            #   tensorflow.python.ops.gradients_impl._hessian_vector_product
-            proj = [T.sum(gi * disconnected_grad(ui)) for gi, ui in zip(g, u)]
-            proj_grad = [T.grad(proj_elem, args,
+            proj = T.sum(gradient * disconnected_grad(argument_type))
+            Rop = T.grad(proj, argument)
+        return theano.function([argument, argument_type], Rop,
+                               on_unused_input="ignore")
+
+    @staticmethod
+    def _compute_nary_hessian_vector_product(gradients, arguments):
+        """Returns a function accepting `2 * len(arguments)` arguments to
+        compute a Hessian-vector product of a multivariate function.
+
+        Notes
+        -----
+        The implementation is based on TensorFlow's '_hessian_vector_product'
+        function in 'tensorflow.python.ops.gradients_impl'.
+        """
+        argument_types = [argument.type() for argument in arguments]
+        try:
+            Rop = T.Rop(gradients, arguments, argument_types)
+        except NotImplementedError:
+            proj = [T.sum(gradient * disconnected_grad(argument_type))
+                    for gradient, argument_type in zip(gradients,
+                                                       argument_types)]
+            proj_grad = [T.grad(proj_elem, arguments,
                                 disconnected_inputs="ignore",
                                 return_disconnected="None")
                          for proj_elem in proj]
@@ -99,28 +93,33 @@ class TheanoBackend(Backend):
             proj_grad_stack = [
                 T.stacklists([c for c in row if c is not None])
                 for row in proj_grad_transpose]
-            R = [T.sum(stack, axis=0) for stack in proj_grad_stack]
-        return theano.function(list(itertools.chain(args, u)), R,
-                               on_unused_input="warn")
+            Rop = [T.sum(stack, axis=0) for stack in proj_grad_stack]
+        return theano.function(
+            list(itertools.chain(arguments, argument_types)), Rop,
+            on_unused_input="warn")
 
     @assert_backend_available
-    def compute_hessian(self, func, argument):
+    def compute_hessian(self, function, arguments):
+        """Computes the directional derivative of the gradient, which is
+        equivalent to computing a Hessian-vector product with the direction
+        vector.
         """
-        Computes the directional derivative of the gradient (which is equal to
-        the Hessian multiplied by direction).
-        """
-        # See above.
-        args = flatten_args(argument)
-        if len(args) == 1:
-            (arg,) = args
-            g = T.grad(func, arg)
-            return self._compute_unary_hvp(g, arg)
+        flattened_arguments = flatten_args(arguments)
+        if len(flattened_arguments) == 1:
+            (argument,) = flattened_arguments
+            gradient = T.grad(function, argument)
+            return self._compute_unary_hessian_vector_product(
+                gradient, argument)
 
-        g = T.grad(func, args)
-        hess = self._compute_nary_hvp(g, args)
-        def wrapper(x, u):
-            return hess(*itertools.chain(flatten_args(x), flatten_args(u)))
-        return group_return_values(wrapper, argument)
+        gradients = T.grad(function, flattened_arguments)
+        hessian_vector_product = self._compute_nary_hessian_vector_product(
+            gradients, flattened_arguments)
+
+        def wrapper(point, vector):
+            return hessian_vector_product(
+                *itertools.chain(flatten_args(point), flatten_args(vector)))
+
+        return group_return_values(wrapper, arguments)
 
 
-Theano = make_graph_backend_decorator(TheanoBackend)
+Theano = make_graph_backend_decorator(_TheanoBackend)
