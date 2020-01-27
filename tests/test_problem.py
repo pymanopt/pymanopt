adcd0ff8b43514684c4acc48f0c97831e3b6e2f7
Niklas Koep <niklas.koep@gmail.com>
2020-01-27 18:35:11 +0100

    Fix failing test due to API mismatch
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -5,21 +5,26 @@ from numpy import random as rnd
 import numpy.testing as np_testing
 import theano.tensor as T
 
-from pymanopt import Problem, TheanoFunction
+import pymanopt
 from pymanopt.manifolds import Sphere
 
 
 class TestProblem(unittest.TestCase):
     def setUp(self):
         self.X = X = T.vector()
-        self.cost = TheanoFunction(T.exp(T.sum(X**2)), X)
+
+        @pymanopt.function.Theano(X)
+        def cost(X):
+            return T.exp(T.sum(X**2))
+
+        self.cost = cost
 
         n = self.n = 15
 
         self.man = Sphere(n)
 
     def test_prepare(self):
-        problem = Problem(self.man, self.cost)
+        problem = pymanopt.Problem(self.man, self.cost)
         x = rnd.randn(self.n)
         np_testing.assert_allclose(2 * x * np.exp(np.sum(x ** 2)),
                                    problem.egrad(x))
