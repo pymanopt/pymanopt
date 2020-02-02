86d9126936f3703bc8063b54cb4cb10a4f3600ef
Niklas Koep <niklas.koep@gmail.com>
2020-02-02 07:35:54 +0100

    _autograd.py: fix bug in hvp for nary functions
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/autodiff/backends/_autograd.py
+++ pymanopt/autodiff/backends/_autograd.py
@@ -5,6 +5,7 @@ import functools
 
 try:
     import autograd
+    import autograd.numpy as np
 except ImportError:
     autograd = None
 
@@ -38,10 +39,22 @@ class _AutogradBackend(Backend):
             return unpack_singleton_sequence_return_value(gradient)
         return gradient
 
+    @staticmethod
+    def _hessian_vector_product(function, argnum):
+        gradient = autograd.grad(function, argnum)
+
+        def vector_dot_gradient(*args):
+            arguments, vectors = args[:-1], args[-1]
+            gradients = gradient(*arguments)
+            return np.sum(
+                [np.tensordot(gradient, vector, axes=vector.ndim)
+                 for gradient, vector in zip(gradients, vectors)])
+        return autograd.grad(vector_dot_gradient, argnum)
+
     @Backend._assert_backend_available
     def compute_hessian_vector_product(self, function, arguments):
         num_arguments = len(arguments)
-        hessian_vector_product = autograd.hessian_vector_product(
+        hessian_vector_product = self._hessian_vector_product(
             function, argnum=tuple(range(num_arguments)))
 
         @functools.wraps(hessian_vector_product)
