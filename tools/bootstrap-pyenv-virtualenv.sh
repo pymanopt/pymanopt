4cbc1fd1071df990d0a0e3cdb4a7794735afd0ea
Niklas Koep <niklas.koep@gmail.com>
2021-10-29 01:36:04 +0200

    Move requirements files to separate directory
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tools/bootstrap-pyenv-virtualenv.sh
+++ tools/bootstrap-pyenv-virtualenv.sh
@@ -14,8 +14,7 @@ for p in pyenv; do
   command -v $p >/dev/null 2>&1 || { die "\`$p\` not found in PATH"; }
 done
 
-CONFIGURE_OPTS=--enable-shared pyenv install 3.7.6
-pyenv virtualenv 3.7.6 pymanopt-3.7
-pip install --upgrade pip setuptools wheel
-pip install -r dev-requirements.txt
-pip install -r requirements.txt
+CONFIGURE_OPTS=--enable-shared pyenv install 3.7.10
+pyenv virtualenv 3.7.10 pymanopt-3.7.10
+pip install pip==21.3.1 setuptools==47.1.0 wheel==0.37.0
+pip install -r requirements/base.txt -r requirements/dev.txt
