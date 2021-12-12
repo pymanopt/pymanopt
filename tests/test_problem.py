8cb7cf7cb01c007aecc3f497bdc4e44d05124d75
Niklas Koep <niklas.koep@gmail.com>
2021-12-12 19:51:18 +0100

    Add test case for #140
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 from numpy import random as rnd, testing as np_testing
 
 import pymanopt
-from pymanopt.manifolds import Sphere
+from pymanopt.manifolds import Product, Sphere, Stiefel
+from pymanopt.solvers import TrustRegions
 from ._test import TestCase
 
 
@@ -33,3 +34,18 @@ class TestProblem(TestCase):
         problem.verbosity = 2
         with self.assertRaises(AttributeError):
             problem.manifold = None
+
+    def test_vararg_cost_on_product(self):
+        shape = (3, 3)
+        manifold = Product([Stiefel(*shape)] * 2)
+
+        @pymanopt.function.TensorFlow(manifold)
+        def cost(*args):
+            X, Y = args
+            return tf.reduce_sum(X) + tf.reduce_sum(Y)
+
+        problem = pymanopt.Problem(manifold=manifold, cost=cost)
+        solver = TrustRegions(maxiter=1)
+        Xopt, Yopt = solver.solve(problem)
+        self.assertEqual(Xopt.shape, (3, 3))
+        self.assertEqual(Yopt.shape, (3, 3))
