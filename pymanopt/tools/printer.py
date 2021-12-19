079a6bfd7084d3f9c707891109f00eca16dd990c
Niklas Koep <niklas.koep@gmail.com>
2021-12-19 19:55:32 +0100

    Complete docstrings of printer module
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/tools/printer.py
+++ pymanopt/tools/printer.py
@@ -2,17 +2,26 @@ from typing import Any, Iterable, List, Optional, Tuple
 
 
 class VoidPrinter:
-    """Printer that prints its arguments to the void."""
+    """Printer that prints nothing."""
 
     def print_header(self):
-        pass
+        """Print nothing."""
 
     def print_row(self, values: Iterable[Any]):
-        pass
+        """Print nothing.
+
+        Args:
+            values: The values not to print.
+        """
 
 
 def print_list(values: List[str]):
-    """Join and print the values given in the ``values`` list."""
+    """Print a formatted list of values.
+
+    Join and print the values given in the ``values`` list.
+    Args:
+        values: A list of values to join and print.
+    """
     print("".join(values))
 
 
@@ -24,7 +33,18 @@ class ColumnPrinter(VoidPrinter):
         placeholder_values: Placeholder values to use when calculating the
             column widths.
         column_padding: Number of spaces to insert between columns.
+
+    Attributes:
+        column_names: Tuple of column names (headers).
+        column_formatters: Tuple of column formatting strings.
+        column_padding: The number of spaces used to pad columns.
+        column_widths: Tuple of calculated column widths.
     """
+
+    column_names: Tuple[str]
+    column_formatters: Tuple[str]
+    column_padding: int
+    column_widths: Tuple[str]
 
     def __init__(
         self,
@@ -33,25 +53,29 @@ class ColumnPrinter(VoidPrinter):
         placeholder_values: Optional[List[Any]] = None,
         column_padding: int = 4,
     ):
-        self.column_names, format_strings = zip(*columns)
-        self.column_formatters = [
-            f"{{value:{format_string}}}" for format_string in format_strings
-        ]
+        self.column_names, format_strings = map(tuple, zip(*columns))
+        self.column_formatters = tuple(
+            [f"{{value:{format_string}}}" for format_string in format_strings]
+        )
         self.column_padding = column_padding
 
         # Compute the column widths.
         if placeholder_values is None:
             placeholder_values = [0] * len(self.column_names)
-        self.column_widths = [
-            max(
-                len(column),
-                len(formatter.format(value=value)),
-            )
-            + self.column_padding
-            for column, formatter, value in zip(
-                self.column_names, self.column_formatters, placeholder_values
-            )
-        ]
+        self.column_widths = tuple(
+            [
+                max(
+                    len(column),
+                    len(formatter.format(value=value)),
+                )
+                + self.column_padding
+                for column, formatter, value in zip(
+                    self.column_names,
+                    self.column_formatters,
+                    placeholder_values,
+                )
+            ]
+        )
 
     def print_header(self):
         """Print a formatted header line."""
@@ -70,7 +94,11 @@ class ColumnPrinter(VoidPrinter):
         print_list(underline_segments)
 
     def print_row(self, values: Iterable[Any]):
-        """Print ``values`` formatted as a row."""
+        """Print formatted as a row.
+
+        Args:
+            values: The values to print.
+        """
         column_strings = [
             formatter.format(value=value)
             + " " * (column_width - len(formatter.format(value=value)))
