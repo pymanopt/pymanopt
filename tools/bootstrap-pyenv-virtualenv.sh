b5aca143dea3be1456753f259cbcb0d1f15b49e9
Niklas Koep <niklas.koep@gmail.com>
2021-12-31 11:15:41 +0100

    Bump python version in bootstrap script
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tools/bootstrap-pyenv-virtualenv.sh
+++ tools/bootstrap-pyenv-virtualenv.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # With `pyenv` and its `pyenv-virtualenv` plugin installed, this script
-# bootstraps a Python 3.7 development environment.
+# bootstraps a Python development environment.
 
 die() {
   echo "$@"
@@ -14,7 +14,7 @@ for p in pyenv pyenv-virtualenv; do
   command -v $p >/dev/null 2>&1 || { die "\`$p\` not found in PATH"; }
 done
 
-PYTHON_VERSION="3.7.10"
+PYTHON_VERSION="3.9.9"
 NAME="pymanopt"
 ENV_NAME="$NAME-$PYTHON_VERSION"
 
@@ -22,5 +22,7 @@ CONFIGURE_OPTS=--enable-shared pyenv install -s "$PYTHON_VERSION"
 pyenv virtualenv -f "$PYTHON_VERSION" "$ENV_NAME"
 pyenv local "$ENV_NAME"
 pip install -r requirements/ci.txt
-pip install -r requirements/base.txt -r requirements/dev.txt
-
+pip install \
+  -r requirements/base.txt \
+  -r requirements/dev.txt \
+  -r requirements/docs.txt
