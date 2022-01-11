b58a78bf024b7f27c280da7ece4a7c5ad381ee13
Niklas Koep <niklas.koep@gmail.com>
2022-01-11 22:35:51 +0100

    Rename arguments in stiefel.py
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/manifolds/stiefel.py
+++ pymanopt/manifolds/stiefel.py
@@ -38,87 +38,101 @@ class Stiefel(EuclideanEmbeddedSubmanifold):
     def typicaldist(self):
         return np.sqrt(self._p * self._k)
 
-    def inner(self, X, G, H):
-        # Inner product (Riemannian metric) on the tangent space
-        # For the stiefel this is the Frobenius inner product.
-        return np.tensordot(G, H, axes=G.ndim)
+    def inner(self, point, tangent_vector_a, tangent_vector_b):
+        return np.tensordot(
+            tangent_vector_a, tangent_vector_b, axes=tangent_vector_a.ndim
+        )
 
-    def proj(self, X, U):
-        return U - multiprod(X, multisym(multiprod(multitransp(X), U)))
+    def proj(self, point, vector):
+        return vector - multiprod(
+            point, multisym(multiprod(multitransp(point), vector))
+        )
 
-    # TODO(nkoep): Implement the weingarten map instead.
-    def ehess2rhess(self, X, egrad, ehess, H):
-        XtG = multiprod(multitransp(X), egrad)
+    def ehess2rhess(
+        self, point, euclidean_gradient, euclidean_hvp, tangent_vector
+    ):
+        XtG = multiprod(multitransp(point), euclidean_gradient)
         symXtG = multisym(XtG)
-        HsymXtG = multiprod(H, symXtG)
-        return self.proj(X, ehess - HsymXtG)
+        HsymXtG = multiprod(tangent_vector, symXtG)
+        return self.proj(point, euclidean_hvp - HsymXtG)
 
-    # Retract to the Stiefel using the qr decomposition of X + G.
-    def retr(self, X, G):
+    def retr(self, point, tangent_vector):
         if self._k == 1:
-            # Calculate 'thin' qr decomposition of X + G
-            q, r = np.linalg.qr(X + G)
-            # Unflip any flipped signs
-            XNew = q @ np.diag(np.sign(np.sign(np.diag(r)) + 0.5))
-        else:
-            XNew = X + G
-            for i in range(self._k):
-                q, r = np.linalg.qr(XNew[i])
-                XNew[i] = q @ np.diag(np.sign(np.sign(np.diag(r)) + 0.5))
-        return XNew
+            q, r = np.linalg.qr(point + tangent_vector)
+            return q @ np.diag(np.sign(np.sign(np.diag(r)) + 0.5))
 
-    def norm(self, X, G):
-        # Norm on the tangent space of the Stiefel is simply the Euclidean
-        # norm.
-        return np.linalg.norm(G)
+        target_point = point + tangent_vector
+        for i in range(self._k):
+            q, r = np.linalg.qr(target_point[i])
+            target_point[i] = q @ np.diag(np.sign(np.sign(np.diag(r)) + 0.5))
+        return target_point
 
-    # Generate random Stiefel point using qr of random normally distributed
-    # matrix.
+    def norm(self, point, tangent_vector):
+        return np.linalg.norm(tangent_vector)
+
     def rand(self):
         if self._k == 1:
-            X = np.random.randn(self._n, self._p)
-            q, r = np.linalg.qr(X)
+            matrix = np.random.randn(self._n, self._p)
+            q, _ = np.linalg.qr(matrix)
             return q
 
-        X = np.zeros((self._k, self._n, self._p))
+        point = np.zeros((self._k, self._n, self._p))
         for i in range(self._k):
-            X[i], r = np.linalg.qr(np.random.randn(self._n, self._p))
-        return X
+            point[i], _ = np.linalg.qr(np.random.randn(self._n, self._p))
+        return point
 
-    def randvec(self, X):
-        U = np.random.randn(*np.shape(X))
-        U = self.proj(X, U)
-        U = U / np.linalg.norm(U)
-        return U
+    def randvec(self, point):
+        vector = np.random.randn(*np.shape(point))
+        vector = self.proj(point, vector)
+        return vector / np.linalg.norm(vector)
 
-    def transp(self, x1, x2, d):
-        return self.proj(x2, d)
+    def transp(self, point_a, point_b, tangent_vector_a):
+        return self.proj(point_b, tangent_vector_a)
 
-    def exp(self, X, U):
+    def exp(self, point, tangent_vector):
         if self._k == 1:
             W = expm(
-                np.bmat([[X.T @ U, -U.T @ U], [np.eye(self._p), X.T @ U]])
-            )
-            Z = np.bmat([[expm(-X.T @ U)], [np.zeros((self._p, self._p))]])
-            Y = np.bmat([X, U]) @ W @ Z
-        else:
-            Y = np.zeros(np.shape(X))
-            for i in range(self._k):
-                W = expm(
-                    np.bmat(
+                np.bmat(
+                    [
                         [
-                            [X[i].T @ U[i], -U[i].T @ U[i]],
-                            [np.eye(self._p), X[i].T @ U[i]],
-                        ]
-                    )
+                            point.T @ tangent_vector,
+                            -tangent_vector.T @ tangent_vector,
+                        ],
+                        [np.eye(self._p), point.T @ tangent_vector],
+                    ]
                 )
-                Z = np.bmat(
-                    [[expm(-X[i].T @ U[i])], [np.zeros((self._p, self._p))]]
+            )
+            Z = np.bmat(
+                [
+                    [expm(-point.T @ tangent_vector)],
+                    [np.zeros((self._p, self._p))],
+                ]
+            )
+            return np.bmat([point, tangent_vector]) @ W @ Z
+
+        Y = np.zeros_like(point)
+        for i in range(self._k):
+            W = expm(
+                np.bmat(
+                    [
+                        [
+                            point[i].T @ tangent_vector[i],
+                            -tangent_vector[i].T @ tangent_vector[i],
+                        ],
+                        [np.eye(self._p), point[i].T @ tangent_vector[i]],
+                    ]
                 )
-                Y[i] = np.bmat([X[i], U[i]]) @ W @ Z
+            )
+            Z = np.bmat(
+                [
+                    [expm(-point[i].T @ tangent_vector[i])],
+                    [np.zeros((self._p, self._p))],
+                ]
+            )
+            Y[i] = np.bmat([point[i], tangent_vector[i]]) @ W @ Z
         return Y
 
-    def zerovec(self, X):
+    def zerovec(self, point):
         if self._k == 1:
             return np.zeros((self._n, self._p))
         return np.zeros((self._k, self._n, self._p))
