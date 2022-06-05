41531c12af92b395c67478a80b45f35c86697f20
Niklas Koep <niklas.koep@gmail.com>
2022-06-05 13:15:11 +0200

    Remove unnecessary regression calculation
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- tests/manifolds/_manifold_tests.py
+++ tests/manifolds/_manifold_tests.py
@@ -1,5 +1,4 @@
 import autograd.numpy as np
-import scipy.stats
 
 import pymanopt
 from pymanopt.tools import diagnostics
@@ -19,20 +18,12 @@ class ManifoldTestCase(TestCase):
 
     def run_gradient_approximation_test(self):
         problem = pymanopt.Problem(self.manifold, self.cost)
-        # Approximate the cost function with a 1st order model.
-        h, _, segment, poly = diagnostics.check_directional_derivative(problem)
-        x = np.log(h[segment])
-        y = np.log(10) * np.polyval(poly, np.log10(np.e) * x)
-        slope = scipy.stats.linregress(x, y).slope
+        *_, (slope, *_) = diagnostics.check_directional_derivative(problem)
         assert 1.95 <= slope <= 2.05
 
     def run_hessian_approximation_test(self):
         problem = pymanopt.Problem(self.manifold, self.cost)
-        # Approximate the cost function with a 2nd order model.
-        h, error, segment, poly = diagnostics.check_directional_derivative(
+        _, error, _, (slope, *_) = diagnostics.check_directional_derivative(
             problem, use_quadratic_model=True
         )
-        x = np.log(h[segment])
-        y = np.log(10) * np.polyval(poly, np.log10(np.e) * x)
-        slope = scipy.stats.linregress(x, y).slope
         assert np.allclose(np.linalg.norm(error), 0) or (2.95 <= slope <= 3.05)
