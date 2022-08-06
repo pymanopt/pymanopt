20b39d9735eb4fcc5d1c5ae844f1af72915025ad
Niklas Koep <niklas.koep@gmail.com>
2022-08-06 09:37:35 +0200

    Move isort config back to pyproject.toml
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- pymanopt/__init__.py
+++ pymanopt/__init__.py
@@ -7,6 +7,7 @@ from pymanopt.core.problem import Problem
 
 from . import _version
 
+
 __version__ = _version.get_versions()["version"]
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = os.getenv("TF_CPP_MIN_LOG_LEVEL", "2")
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 import versioneer
 
+
 if __name__ == "__main__":
     setuptools.setup(
         version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass()
