75ce0e551aaaf9ac46c84c834f87f0f121c785d2
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-10-16 14:30:02 +0200

    uniformize types in numerics

--- src/pymanopt/numerics/_backends/numpy.py
+++ src/pymanopt/numerics/_backends/numpy.py
@@ -2,16 +2,16 @@ import numpy as np
 
 import pymanopt.numerics.core as nx
 
-generic_np_type = int | float | np.generic | np.ndarray
+generic_np_type = int | float | tuple | list | np.generic | np.ndarray
 
 
 @nx.abs.register
-def _(array: np.ndarray) -> np.ndarray:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.abs(array)
 
 
 @nx.all.register
-def _(array: np.ndarray) -> bool:
+def _(array: generic_np_type) -> bool:
     return np.all(array)
 
 
@@ -21,7 +21,7 @@ def _(array_a: generic_np_type, array_b: generic_np_type) -> bool:
 
 
 @nx.any.register
-def _(array: list | np.ndarray) -> bool:
+def _(array: generic_np_type) -> bool:
     return np.any(array)
 
 
@@ -31,37 +31,37 @@ def _(start: int, stop: int = None, step: int = 1) -> np.ndarray:
 
 
 @nx.arccos.register
-def _(array: generic_np_type) -> generic_np_type:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.arccos(array)
 
 
 @nx.arccosh.register
-def _(array: generic_np_type) -> generic_np_type:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.arccosh(array)
 
 
 @nx.arctan.register
-def _(array: generic_np_type) -> generic_np_type:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.arctan(array)
 
 
 @nx.arctanh.register
-def _(array: generic_np_type) -> generic_np_type:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.arctanh(array)
 
 
 @nx.exp.register
-def _(array: np.ndarray) -> np.ndarray:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.exp(array)
 
 
 @nx.tensordot.register
 def _(
-    array_a: np.ndarray, array_b: np.ndarray, *, axes: int = 2
-) -> np.ndarray:
+    array_a: generic_np_type, array_b: generic_np_type, *, axes: int = 2
+) -> np.float64 | np.ndarray:
     return np.tensordot(array_a, array_b, axes=axes)
 
 
 @nx.tanh.register
-def _(array: np.ndarray) -> np.ndarray:
+def _(array: generic_np_type) -> np.float64 | np.ndarray:
     return np.tanh(array)
