9afbba7e0a5e24c47123f8af2486dbbf59ecb27e
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-11-02 07:52:50 +0100

    WIP: test torch backend

--- tests/numerics/test_core.py
+++ tests/numerics/test_core.py
@@ -253,15 +253,16 @@ def test_exp(argument, expected_output):
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (np.array([4, 2]), 0),
-        (np.array([4, 2]), (0, 1)),
+        (np.array([4, 2]), 0, np.expand_dims(np.array([4, 2]), axis=0)),
+        (np.array([4, 2]), (0, 1), np.expand_dims(np.array([4, 2]), axis=(0, 1))),
     ]
 )
-def test_expand_dims(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_expand_dims(argument_a, argument_b, expected_output):
     output = nx.expand_dims(argument_a, argument_b)
-    assert nx.allclose(output, np.expand_dims(argument_a, argument_b))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
@@ -271,6 +272,7 @@ def test_expand_dims(argument_a, argument_b):
         np.complex128,
     ]
 )
+@_test_numerics_supported_backends
 def test_finfo(argument):
     assert nx.finfo(argument) == np.finfo(argument)
 
@@ -280,64 +282,72 @@ def test_float64():
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        [1, 2],
-        [np.array([1, 2]), np.array([3, 4])],
+        ([1, 2], np.hstack([1, 2])),
+        (
+            [np.array([1, 2]), np.array([3, 4])],
+            np.hstack([np.array([1, 2]), np.array([3, 4])])
+        ),
     ]
 )
-def test_hstack(argument):
+@_test_numerics_supported_backends
+def test_hstack(argument, expected_output):
     output = nx.hstack(argument)
-    assert nx.allclose(output, np.hstack(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        1,
-        1 + 2j,
-        np.array([1 + 1j, 2]),
+        (1, False),
+        (1 + 2j, True),
+        (np.array([1 + 1j, 2]), True),
     ]
 )
-def test_iscomplexobj(argument):
+@_test_numerics_supported_backends
+def test_iscomplexobj(argument, expected_output):
     output = nx.iscomplexobj(argument)
-    assert nx.allclose(output, np.iscomplexobj(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        np.nan,
-        np.array([np.nan, 1]),
-        np.array([1, 2]),
+        (np.nan, True),
+        (np.array([np.nan, 1]), True),
+        (np.array([1, 2]), False),
     ]
 )
-def test_isnan(argument):
+@_test_numerics_supported_backends
+def test_isnan(argument, expected_output):
     output = nx.isnan(argument)
-    assert nx.allclose(output, np.isnan(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        3,
-        np.array([1, 2]),
+        (3, np.log(3)),
+        (np.array([1, 2]), np.log(np.array([1, 2]))),
     ]
 )
-def test_log(argument):
+@_test_numerics_supported_backends
+def test_log(argument, expected_output):
     output = nx.log(argument)
-    assert nx.allclose(output, np.log(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        (3, 6, 8)
+        ((3, 6, 8), np.logspace(3, 6, 8)),
     ]
 )
-def test_logspace(argument):
+@_test_numerics_supported_backends
+def test_logspace(argument, expected_output):
     output = nx.logspace(*argument)
-    assert nx.allclose(output, np.logspace(*argument))
+    assert nx.allclose(output, expected_output)
 
 
 def test_newaxis():
@@ -345,15 +355,16 @@ def test_newaxis():
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        1,
-        (3, 2)
+        (1, np.ones(1)),
+        ((3, 2), np.ones((3, 2))),
     ]
 )
-def test_ones(argument):
+@_test_numerics_supported_backends
+def test_ones(argument, expected_output):
     output = nx.ones(argument)
-    assert nx.allclose(output, np.ones(argument))
+    assert nx.allclose(output, expected_output)
 
 
 def test_pi():
@@ -361,153 +372,175 @@ def test_pi():
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (np.array([1.2, 2]), np.array([3, 4])),
+        (
+            np.array([1.2, 2]),
+            np.array([3, 4]),
+            np.polyfit(
+                np.array([1.2, 2]), np.array([3, 4]), 1
+            )
+        ),
     ]
 )
-def test_polyfit(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_polyfit(argument_a, argument_b, expected_output):
     output = nx.polyfit(argument_a, argument_b, 1)
-    assert nx.allclose(output, np.polyfit(argument_a, argument_b, 1))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (np.array([1.2, 2]), np.array([3, 4])),
+        (
+            np.array([1.2, 2]),
+            np.array([3, 4]),
+            np.polyval(np.array([1.2, 2]), np.array([3, 4]))
+        ),
     ]
 )
-def test_polyval(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_polyval(argument_a, argument_b, expected_output):
     output = nx.polyval(argument_a, argument_b)
-    assert nx.allclose(output, np.polyval(argument_a, argument_b))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        np.array([1.2, 2]),
+        (np.array([1.2, 2]), 2.4),
     ]
 )
-def test_prod(argument):
+@_test_numerics_supported_backends
+def test_prod(argument, expected_output):
     output = nx.prod(argument)
-    assert nx.allclose(output, np.prod(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        1,
-        1 + 2j,
-        np.array([1 + 1j, 2]),
+        (1, 1),
+        (1 + 2j, 1),
+        (np.array([1 + 1j, 2]), np.array([1, 2])),
     ]
 )
-def test_real(argument):
+@_test_numerics_supported_backends
+def test_real(argument, expected_output):
     output = nx.real(argument)
-    assert nx.allclose(output, np.real(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        2,
-        np.array([1, 2]),
+        (2, np.sin(2)),
+        (np.array([1, 2]), np.sin(np.array([1, 2]))),
     ]
 )
-def test_sin(argument):
+@_test_numerics_supported_backends
+def test_sin(argument, expected_output):
     output = nx.sin(argument)
-    assert nx.allclose(output, np.sin(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        2,
-        np.array([1, 2]),
+        (2, np.sinc(2)),
+        (np.array([1, 2]), np.sinc(np.array([1, 2]))),
     ]
 )
-def test_sinc(argument):
+@_test_numerics_supported_backends
+def test_sinc(argument, expected_output):
     output = nx.sinc(argument)
-    assert nx.allclose(output, np.sinc(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        [2, 1],
-        np.array([2, 1]),
+        ([2, 1], np.sort([2, 1])),
+        (np.array([2, 1]), np.sort(np.array([2, 1]))),
     ]
 )
-def test_sort(argument):
+@_test_numerics_supported_backends
+def test_sort(argument, expected_output):
     output = nx.sort(argument)
-    assert nx.allclose(output, np.sort(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        2.2,
-        [2.2, 1],
-        np.array([2.2, 1]),
+        (2.2, np.spacing(2.2)),
+        ([2.2, 1], np.spacing([2.2, 1])),
+        (np.array([2.2, 1]), np.spacing(np.array([2.2, 1]))),
     ]
 )
-def test_spacing(argument):
+@_test_numerics_supported_backends
+def test_spacing(argument, expected_output):
     output = nx.spacing(argument)
-    assert nx.allclose(output, np.spacing(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        2,
-        [2, 1],
-        np.array([2, 1]),
+        (2, np.sqrt(2)),
+        ([2, 1], np.sqrt([2, 1])),
+        (np.array([2, 1]), np.sqrt(np.array([2, 1]))),
     ]
 )
-def test_sqrt(argument):
+@_test_numerics_supported_backends
+def test_sqrt(argument, expected_output):
     output = nx.sqrt(argument)
-    assert nx.allclose(output, np.sqrt(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (1, None),
-        ([1, 2], None),
-        (np.array([1, 2]), None),
-        (np.array([[1, 2], [3, 4]]), 1),
+        (1, None, 1),
+        ([1, 2], None, 3),
+        (np.array([1, 2]), None, 3),
+        (np.array([[1, 2], [3, 4]]), 1, np.sum(np.array([[1, 2], [3, 4]]), 1)),
     ]
 )
-def test_sum(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_sum(argument_a, argument_b, expected_output):
     if argument_b is None:
         output = nx.sum(argument_a)
-        assert nx.allclose(output, np.sum(argument_a))
     else:
         output = nx.sum(argument_a, argument_b)
-        assert nx.allclose(output, np.sum(argument_a, argument_b))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (1, [1, 2]),
-        ([1, 2], [3, 4, 1]),
-        (np.array([1, 2]), np.array([3, 4, 1])),
+        (1, [1, 2], np.tile(1, [1, 2])),
+        ([1, 2], [3, 4, 1], np.tile([1, 2], [3, 4, 1])),
+        (
+            np.array([1, 2]),
+            np.array([3, 4, 1]),
+            np.tile(np.array([1, 2]), np.array([3, 4, 1]))
+        ),
     ]
 )
-def test_tile(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_tile(argument_a, argument_b, expected_output):
     output = nx.tile(argument_a, argument_b)
-    assert nx.allclose(output, np.tile(argument_a, argument_b))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
     "argument, expected_output",
     [
         (np.arctanh(np.array([0.4, 0.2])), np.array([0.4, 0.2])),
-        (jnp.arctanh(jnp.array([0.4, 0.2])), jnp.array([0.4, 0.2])),
-        (torch.arctanh(torch.Tensor([0.4, 0.2])), torch.Tensor([0.4, 0.2])),
-        (tf.math.atanh(tf.constant([0.4, 0.2])), tf.constant([0.4, 0.2])),
     ],
 )
+@_test_numerics_supported_backends
 def test_tanh(argument, expected_output):
     output = nx.tanh(argument)
     assert nx.allclose(output, expected_output)
@@ -517,44 +550,47 @@ def test_tanh(argument, expected_output):
     "argument_a, argument_b, expected_output",
     [
         (np.array([-4, 2]), np.array([1, 3]), 2),
-        (jnp.array([-4, 2]), jnp.array([1, 3]), 2),
-        (torch.Tensor([-4, 2]), torch.Tensor([1, 3]), 2),
-        (tf.constant([-4, 2]), tf.constant([1, 3]), 2),
     ],
 )
+@_test_numerics_supported_backends
 def test_tensordot(argument_a, argument_b, expected_output):
     output = nx.tensordot(argument_a, argument_b, axes=argument_a.ndim)
     assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument",
+    "argument, expected_output",
     [
-        [[1, 2], [3, 4]],
-        np.array([[1, 2], [3, 4]]),
+        ([[1, 2], [3, 4]], np.trace([[1, 2], [3, 4]])),
+        (np.array([[1, 2], [3, 4]]), np.trace(np.array([[1, 2], [3, 4]]))),
     ]
 )
-def test_trace(argument):
+@_test_numerics_supported_backends
+def test_trace(argument, expected_output):
     output = nx.trace(argument)
-    assert nx.allclose(output, np.trace(argument))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (2, None),
-        ([1, 2], None),
-        (np.array([1, 2]), None),
-        (np.array([[1, 2], [3, 4]]), (1, 0)),
+        (2, None, 2),
+        ([1, 2], None, np.transpose([1, 2])),
+        (np.array([1, 2]), None, np.transpose(np.array([1, 2]))),
+        (
+            np.array([[1, 2], [3, 4]]),
+            (1, 0),
+            np.transpose(np.array([[1, 2], [3, 4]]), (1, 0))
+        ),
     ]
 )
-def test_transpose(argument_a, argument_b):
+@_test_numerics_supported_backends
+def test_transpose(argument_a, argument_b, expected_output):
     if argument_b is None:
         output = nx.transpose(argument_a)
-        assert nx.allclose(output, np.transpose(argument_a))
     else:
         output = nx.transpose(argument_a, argument_b)
-        assert nx.allclose(output, np.transpose(argument_a, argument_b))
+    assert nx.allclose(output, expected_output)
 
 
 @pytest.mark.parametrize(
@@ -564,6 +600,7 @@ def test_transpose(argument_a, argument_b):
         (2, 1, 1),
     ]
 )
+@_test_numerics_supported_backends
 def test_triu_indices(argument_a, argument_b, argument_c):
     if argument_c is None:
         output = nx.triu_indices(argument_a, argument_b)
@@ -583,6 +620,7 @@ def test_triu_indices(argument_a, argument_b, argument_c):
             np.array([[1, 2], [3, 4]]), np.array([[1, 2], [3, 4]])]),
     ]
 )
+@_test_numerics_supported_backends
 def test_vectorize(argument_a, argument_b, argument_c):
     output = nx.vectorize(argument_a, signature=argument_b)
     assert nx.allclose(
@@ -599,6 +637,7 @@ def test_vectorize(argument_a, argument_b, argument_c):
         [np.array([2, 1]), np.array([2, 1])],
     ]
 )
+@_test_numerics_supported_backends
 def test_vstack(argument):
     output = nx.vstack(argument)
     assert nx.allclose(output, np.vstack(argument))
@@ -612,6 +651,7 @@ def test_vstack(argument):
         np.array([False, True]),
     ]
 )
+@_test_numerics_supported_backends
 def test_where(argument):
     output = nx.where(argument)
     assert nx.allclose(output, np.where(argument))
@@ -624,6 +664,7 @@ def test_where(argument):
         [2, 1],
     ]
 )
+@_test_numerics_supported_backends
 def test_zeros(argument):
     output = nx.zeros(argument)
     assert nx.allclose(output, np.zeros(argument))
@@ -636,6 +677,7 @@ def test_zeros(argument):
         [2, 1],
     ]
 )
+@_test_numerics_supported_backends
 def test_zeros_like(argument):
     output = nx.zeros_like(argument)
     assert nx.allclose(output, np.zeros_like(argument))
