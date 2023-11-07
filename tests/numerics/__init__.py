f2cae0cd91ec628aa9269e654622940b1340b692
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-11-07 10:27:02 +0100

    test functions in numerics that take as Sequence of arrays

--- tests/numerics/__init__.py
+++ tests/numerics/__init__.py
@@ -1,5 +1,6 @@
 import functools
 import numpy as np
+from typing import Sequence
 
 from pymanopt.numerics import NUMERICS_SUPPORTED_BACKENDS, numpy_to_backend
 
@@ -11,6 +12,15 @@ def _test_numerics_supported_backends(func):
             for key, value in kwargs.items():
                 if type(value) == np.ndarray:
                     kwargs[key] = numpy_to_backend(value, backend)
+                if isinstance(value, Sequence):
+                    new_value = list()
+                    for item in value:
+                        if type(item) == np.ndarray:
+                            new_value.append(numpy_to_backend(item, backend))
+                        else:
+                            new_value.append(item)
+                    kwargs[key] = new_value
+
             func(**kwargs)
 
     return wrapper
