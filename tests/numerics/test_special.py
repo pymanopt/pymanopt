77026e5c6ec2cabfdf79829c10e1916b0442391c
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-11-07 11:03:23 +0100

    test nx.special with all backends

--- tests/numerics/test_special.py
+++ tests/numerics/test_special.py
@@ -5,13 +5,12 @@ import pymanopt.numerics as nx
 
 
 @pytest.mark.parametrize(
-    "argument_a, argument_b",
+    "argument_a, argument_b, expected_output",
     [
-        (2, 0),
-        (3, 2)
+        (2, 0, scipy.special.comb(2, 0)),
+        (3, 2, scipy.special.comb(3, 2)),
     ],
 )
-def test_comb(argument_a, argument_b):
+def test_comb(argument_a, argument_b, expected_output):
     actual = nx.special.comb(argument_a, argument_b)
-    expected = scipy.special.comb(argument_a, argument_b)
-    assert nx.allclose(actual, expected)
+    assert nx.allclose(actual, expected_output)
