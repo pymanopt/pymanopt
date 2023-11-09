9bf6cfbbdda2b8674a997085efa189f841414116
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-11-09 09:43:07 +0100

    put get_backend, numpy_to_backend and array_as in pymanopt.numerics._backends.core

--- src/pymanopt/numerics/__init__.py
+++ src/pymanopt/numerics/__init__.py
@@ -10,6 +10,7 @@ __all__ = [
     "arctanh",
     "argmin",
     "array",
+    "array_as",
     "block",
     "conjugate",
     "cos",
@@ -20,6 +21,7 @@ __all__ = [
     "eye",
     "finfo",
     "float64",
+    "get_backend",
     "hstack",
     "integer",
     "iscomplexobj",
@@ -30,6 +32,7 @@ __all__ = [
     "logspace",
     "ndarray",
     "newaxis",
+    "numpy_to_backend",
     "ones",
     "pi",
     "prod",
@@ -114,6 +117,11 @@ from pymanopt.numerics.core import (
     zeros_like,
 )
 from pymanopt.numerics import linalg, special
+from pymanopt.numerics._backends.core import (
+    array_as,
+    get_backend,
+    numpy_to_backend
+)
 from numpy import random
 
 NUMERICS_SUPPORTED_BACKENDS = ["numpy", "pytorch"]
@@ -125,83 +133,6 @@ def register_backends():
             importlib.import_module(f"pymanopt.numerics._backends.{backend}")
         except ImportError:
             pass
-
-
-def get_backend(point):
-    backend = None
-
-    while isinstance(point, (tuple, list)):
-        point = point[0]
-
-    import numpy as np
-    if isinstance(point, np.ndarray):
-        backend = "numpy"
-
-    try:
-        import torch
-        if isinstance(point, torch.Tensor):
-            backend = "pytorch"
-    except ImportError:
-        pass
-
-    try:
-        import jax.numpy as jnp
-        if isinstance(point, jnp.ndarray):
-            backend = "jax"
-    except ImportError:
-        pass
-
-    try:
-        import tensorflow as tf
-        if isinstance(point, tf.Tensor):
-            backend = "tensorflow"
-    except ImportError:
-        pass
-
-    if backend is None:
-        raise ValueError("Unknown backend...")
-
-    return backend
-
-
-def numpy_to_backend(point, backend):
-    # if point is a namedtuple, convert each point
-    if hasattr(point, '_fields'):
-        point = point.__class__(*[
-            numpy_to_backend(p, backend) for p in point
-        ])
-        return point
-    if isinstance(point, tuple):
-        return tuple(numpy_to_backend(p, backend) for p in point)
-    if isinstance(point, list):
-        return [numpy_to_backend(p, backend) for p in point]
-
-    if point.dtype.kind == 'c':
-        dtype = 'complex128'
-    else:
-        dtype = 'float64'
-
-    if backend == 'numpy':
-        import numpy as np
-        point = np.array(point, dtype=dtype)
-    elif backend == 'pytorch':
-        import torch
-        point = torch.tensor(point, dtype=getattr(torch, dtype))
-    elif backend == 'jax':
-        import jax.numpy as jnp
-        point = jnp.array(point, dtype=dtype)
-    elif backend == 'tensorflow':
-        import tensorflow as tf
-        point = tf.convert_to_tensor(point, dtype=getattr(tf, dtype))
-    else:
-        raise ValueError(f"Unknown backend '{backend}'")
-
-    return point
-
-
-def array_as(point, as_):
-    backend = get_backend(as_)
-    return numpy_to_backend(point, backend)
 
 
 register_backends()
