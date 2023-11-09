bae988b7de0ab5c5dc11c854f161be095b1ae2d2
Antoine Collas <22830806+antoinecollas@users.noreply.github.com>
2023-11-09 16:31:42 +0100

    BackendManifold --> TestBackendManifoldFactory to select on which backend tests should be run

--- tests/manifolds/test_manifold.py
+++ tests/manifolds/test_manifold.py
@@ -4,21 +4,27 @@ from copy import deepcopy
 from pymanopt.numerics import NUMERICS_SUPPORTED_BACKENDS
 
 
-class BackendManifold(type):
-    def __new__(cls, name, bases, attrs):
-        # decorate all tests with the backend setup
-        keys = list(attrs.keys())
-        for key in keys:
-            if key.startswith("test_"):
-                test = deepcopy(attrs[key])
-                for backend in NUMERICS_SUPPORTED_BACKENDS:
-                    attrs[f"{key}_{backend}"] = cls.test_with_backends(cls, test, backend)
-                del attrs[key]
+def TestBackendManifoldFactory(backends=NUMERICS_SUPPORTED_BACKENDS):
+    if not isinstance(backends, (list, tuple)):
+        backends = [backends]
 
-        return super().__new__(cls, name, bases, attrs)
+    class TestBackendManifold(type):
+        def __new__(cls, name, bases, attrs):
+            # decorate all tests with the backend setup
+            keys = list(attrs.keys())
+            for key in keys:
+                if key.startswith("test_"):
+                    test = deepcopy(attrs[key])
+                    for backend in backends:
+                        attrs[f"{key}_{backend}"] = cls.test_with_backends(cls, test, backend)
+                    del attrs[key]
 
-    def test_with_backends(cls, test, backend):
-        def wrapped_test(self, *args, **kwargs):
-            self.manifold._backend = backend
-            test(self, *args, **kwargs)
-        return wrapped_test
+            return super().__new__(cls, name, bases, attrs)
+
+        def test_with_backends(cls, test, backend):
+            def wrapped_test(self, *args, **kwargs):
+                self.manifold._backend = backend
+                test(self, *args, **kwargs)
+            return wrapped_test
+
+    return TestBackendManifold
