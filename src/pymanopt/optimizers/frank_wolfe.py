050f33eaeaadca36c7a731ba44295dc99d200571
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-12-01 14:43:52 -0500

    Add general sub problem handling and add docstring

--- src/pymanopt/optimizers/frank_wolfe.py
+++ src/pymanopt/optimizers/frank_wolfe.py
@@ -3,26 +3,53 @@ from copy import deepcopy
 
 import numpy as np
 
+from pymanopt.manifolds import _PositiveDefiniteBase
 from pymanopt.optimizers.optimizer import Optimizer, OptimizerResult
 from pymanopt.tools import printer
 
 class FrankWolfe(Optimizer):
     """Riemannian Frank Wolfe method.
+
+    Generalization of the Reimmanian Frank-Wolfe method 
+    described in [Weber, Sra, Math. Prog., 2022](@cite WeberSra:2022).
+
+    We aim to solve the following constrained optimization problem:
+    ..math::    
+        \begin{equation}\label{eq:problem_statement}
+            \min _{x \in \mathcal{X} \subseteq \mathcal{M}} \phi(x)
+        \end{equation
+
+    where :math:`\mathcal{M}` is a Riemannian manifold and 
+    :math:`\phi` is a smooth function on :math:`\mathcal{M}`.
+
+    The Frank-Wolfe method is an iterative algorithm that performs
+    a line search in the direction of the negative gradient of :math:`\phi`
+    to find a new iterate :math:`x_{k+1}`. The algorithm is initialized
+    with an initial point :math:`x_0 \in \mathcal{M}` and iterates until
+    a stopping criterion is satisfied.
+    
+    Args:
+        sub_problem: A function that solves the subproblem of the Frank-Wolfe
+            method. If no value is provided and the manifold is Positive Definite
+            matrices then the step direction is computed using the closed-form
+            solution from Theorem 4.1 in [Weber, Sra, Math. Prog., 2022](@cite WeberSra:2022).
+
     """
 
     def __init__(
         self,
+        sub_problem=None,
         *args,
         **kwargs,
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__(sub_problem, *args, **kwargs)
 
     def sgnplus(self, D):
         Dsigns = np.sign(D)
         Dsigns[Dsigns == -1] = 0
         return Dsigns
 
-    def step_direction(self, grad, X, L, U):
+    def step_direction(self, objective, manifold, gradient, grad, cost, X, L, U):
         svdres = np.linalg.svd(grad)
         Q = svdres.U
         D = svdres.S
@@ -37,7 +64,7 @@ class FrankWolfe(Optimizer):
         return np.linalg.inv(X) @ Q @ (Pstar @ self.sgnplus(D) @ P + Lcap) @ Qstar @ np.linalg.inv(X)
 
     def run(
-        self, problem, L, U, *, initial_point=None, reuse_line_searcher=False
+        self, problem, L, U, *, initial_point=None,
     ) -> OptimizerResult:
         """Run FW method.
 
@@ -56,6 +83,11 @@ class FrankWolfe(Optimizer):
         manifold = problem.manifold
         objective = problem.cost
         gradient = problem.riemannian_gradient
+        sub_problem = self.sub_problem
+        if sub_problem is None and _PositiveDefiniteBase.isinstance(problem.manifold):
+            sub_problem = self.step_direction
+        elif sub_problem is None:
+            RuntimeError("No subproblem provided and manifold is not positive definite.")
 
         # If no starting point is specified, generate one at random.
         if initial_point is None:
@@ -91,7 +123,7 @@ class FrankWolfe(Optimizer):
             grad = gradient(x)
             gradient_norm = manifold.norm(x, grad)
 
-            Z = self.step_direction(grad, x, L, U)
+            Z = sub_problem(objective, manifold, gradient, grad, cost, x, L, U)
 
             column_printer.print_row([iteration, cost, gradient_norm])
 
