c784d3d083b9be57e3c29faa21693145140d848f
Tudor Oancea <oancea.tudor@icloud.com>
2024-12-05 10:31:23 +0100

    remove unseless comment
    Signed-off-by: Tudor Oancea <oancea.tudor@icloud.com>

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -16,7 +16,6 @@ from examples import (
 from examples.advanced import check_gradient, check_hessian, check_retraction
 
 
-# TODO: add back tensorflow
 SUPPORTED_BACKENDS = {"numpy", "autograd", "jax", "pytorch", "tensorflow"}
 
 
