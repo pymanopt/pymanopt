a4e02992ad540dd817e9f92df64e7cba853fcbcb
Son Pham-Ba <son.phamba@epfl.ch>
2026-09-18 14:38:07 +0200

    lint

--- tests/test_problem.py
+++ tests/test_problem.py
@@ -25,8 +25,9 @@ class TestProblem:
         problem = pymanopt.Problem(self.manifold, self.cost)
         x = tf.random.normal([self.n])
         np_testing.assert_allclose(
-            2 * x * np.exp(np.sum(x**2)), problem.euclidean_gradient(x),
-            rtol = 2e-7,
+            2 * x * np.exp(np.sum(x**2)),
+            problem.euclidean_gradient(x),
+            rtol=2e-7,
         )
 
     def test_attribute_override(self):
